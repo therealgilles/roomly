b0f047cca64ac8547fd4ff6040a46f18bbbe30d1
Eugene Choe <eugene.choe@gmail.com>
2016-11-02 15:37:09 -0700

    -(frontend) move legacy layout and nav into boilerplate

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header'
+import Nav from '../../components/Nav'
 import './CoreLayout.scss'
 import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
     <Header />
+    <Nav />
     <div className='core-layout__viewport'>
       {children}
     </div>
